d980cea90ec2ccd8a3ba3d65c30158dcb859530d
StevenMarin1227 <steven.marin1227@gmail.com>
2026-03-30 00:08:32 -0500

    Ajuste nuevamente del la URL del backend by: Steven Marin

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://transparencia360.onrender.com",
+  baseURL: "https://transparencia360.onrender.com/api/contratos",
 });
 
 export const obtenerContratos = () => API.get("/contratos");
